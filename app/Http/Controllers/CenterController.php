cb49ae384295779f5c1816e9e2570bccad30efd8
karuneshsf <karuneshmani911@gmail.com>
2023-06-03 01:50:38 +0530

    feat(centers): fixes for doctor screen issues

--- app/Http/Controllers/CenterController.php
+++ app/Http/Controllers/CenterController.php
@@ -9,23 +9,31 @@ use App\Models\Doctors;
 use App\Models\City;
 use App\Models\State;
 use Illuminate\Support\Facades\Log;
-
+use App\Models\DynamicValue;
+use Illuminate\Support\Facades\DB;
 class CenterController extends Controller
 {
     //
     public function index()
     {
-        $centers = Center::all();
+        $centers = DB::table('centers as c')
+        ->select('c.*', 'dym.name as state_name', 'dymc.name as city_name')
+        ->leftJoin('dynamic_values as dym', function($join){
+            $join->on('c.state', '=', 'dym.id')
+                 ->where('dym.parent_id', "7");
+        })
+        ->leftJoin('dynamic_values as dymc', function($join){
+            $join->on('c.city', '=', 'dymc.id')
+                 ->where('dymc.parent_id', "8");
+        })->get();
         $doctor=Doctors::all();
-        $city=City::all();
-        $state = State::all();
-
-        return view('center.index',compact( 'centers','doctor','city','state'));
+        $state=DB::table('dynamic_values as dm')->select('dm.*')->where('dm.parent_id','7')->join('dynamic_mains as dym', 'dm.parent_id', '=', 'dym.id')->get();
+        return view('center.index',compact( 'centers','doctor','state'));
     }
 
     public function addCenter(Request $request){
-
-    $centers=Center::create([
+        // print_r($request->role_id);die;
+        $center=Center::create([
             'centerDetails'=>$request->centerDetails,
             'mobile'=>$request->mobile,
             'alternateMobile'=>$request->alternateMobile,
@@ -34,12 +42,28 @@ class CenterController extends Controller
             'ownerName'=>$request->ownerName,
             'EmailId'=>$request->EmailId,
         ]);
-        if($centers){
+
+        // print_r($center->toArray()); die;
+        if(isset($request->role_id) && isset($center)) {
+            $centerId = $center->id;
+            $ids = $request->role_id;//[34, 56, 100, 104];
+            $doctor= Doctors::whereIn("id", $ids)->update([
+                'center_id' => $centerId
+            ]);
+        }
+        if($center){
             return redirect('centers')->with('status', 'Doctor Created Successfully');
 
         }
         return redirect('centers')->with('status', 'Some Error Occured');
     }
+
+    public function edit(Request $request): JsonResponse {
+        $centerId =  $request->get('centerId');
+        $center=Center::where('id',$centerId)->first();
+        return response()->json(['center' => $center]);
+    }
+
 
     public function destroy(Request $request) : JsonResponse {
         $id = $request->get('id');
